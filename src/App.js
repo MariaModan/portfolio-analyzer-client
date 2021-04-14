12a977cec70795d39b1f3500aad6cde60873b4c1
Maria Modan <maria.modan95@gmail.com>
2021-04-14 20:25:53 +0100

    implement simple table with dummy values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AddSymbol from './components/AddSymbol'
+import PortfolioSummaryTable from './components/PortfolioSummaryTable'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <AddSymbol />
+        <PortfolioSummaryTable />
       </header>
     </div>
   );
